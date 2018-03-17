29044f5422c7f0c0c8a10c551fa03cefa4fad1ef
Eimutisg <eimutis.genys@gmail.com>
2018-03-17 20:19:27 +0000

    Models

---
+++ ProductServer/Models/ProductModels/Supplier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProductServer.Models.ProductModels
+{
+    [Table("Supplier")]
+    public class Supplier
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "SUpplier ID")]
+        public int SupplierID { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+    }
+}
