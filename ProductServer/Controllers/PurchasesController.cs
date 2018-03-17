0f30e7b42bd21c73af52a0cbb41ed55ec17440eb
Eimutisg <eimutis.genys@gmail.com>
2018-03-17 21:36:08 +0000

    Purchases Controller

---
+++ ProductServer/Controllers/PurchasesController.cs
@@ -0,0 +1,46 @@
+﻿using ProductServer.DAL;
+using ProductServer.Models.ProductModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ProductServer.Controllers
+{
+    [Authorize(Roles = "PurchasesManager")]
+    [RoutePrefix("api/Purchases")]
+    public class PurchasesController : ApiController
+    {
+        private SupplierProductRepository context;
+
+        public PurchasesController()
+        {
+            context = new SupplierProductRepository(new ProductDbContext());
+            //prodConfig = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductDTO>());
+            //supConfig = new MapperConfiguration(cfg => cfg.CreateMap<Supplier, SupplierDTO>());
+        }
+
+        // For injection
+        public PurchasesController(SupplierProductRepository ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet]
+        [Route("Suppliers")]
+        public async Task<IList<Supplier>> GetSuppliers()
+        {
+            return await (context as ISupplierRepository).getEntities();
+        }
+
+        [HttpGet]
+        [Route("Products")]
+        public async Task<IList<Product>> GetProducts()
+        {
+            return await (context as IProductRepository).getEntities();
+        }
+    }
+}
